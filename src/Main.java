c2a5cd2afa61b9a32b5f5761b832f3a5d56ff2e4
Jasson Caamal <jasson.adrian.caamal.medina@gmail.com>
2021-04-21 23:22:33 -0500

    Creando paquete UI y agregacion de metodos al UIMENu

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
+import static UI.UIMenu.*;
+
 public class Main {
     public static void main(String[] args) {
         //Doctor myDosctor = new Doctor("pablo");
         Doctor myDosctor = new Doctor();
         myDosctor.name= "Jassso";
-        myDosctor.showName();
-        myDosctor.showId();
+        //myDosctor.showName();
+        //myDosctor.showId();
         System.out.println("**************");
         Doctor otroDoctor = new Doctor();
-        otroDoctor.showId();
+        //otroDoctor.showId();
+        showMenu();
     }
 }
---
+++ src/UI/UIMenu.java
@@ -0,0 +1,64 @@
+package UI;
+
+import java.util.Scanner;
+public class UIMenu {
+
+    public static void showMenu(){
+        int respuesta = 0;
+        System.out.println("***********************************");
+        System.out.println(" Bienbenido al Consultorio medico");
+        System.out.println(" Por favor seleccione una opcion");
+        System.out.println("***********************************");
+        System.out.println(" ");
+        do {
+            System.out.println(" 1.- Doctor");
+            System.out.println(" 2.- Paciente");
+            System.out.println(" 0.- Salir");
+            System.out.println(" ");
+            Scanner sc = new Scanner(System.in);
+            respuesta = Integer.valueOf(sc.nextLine());
+            switch (respuesta){
+                case 1:
+                    System.out.println(" Doctor ");
+                    break;
+                case 2:
+                    respuesta = 0;
+                    showPatientMenu();
+                    break;
+                case 0:
+                    System.out.println(" Gracias por su visita ");
+                    break;
+                default:
+                    System.out.println("Por favor selecione una opcion corecta");
+            }
+        }while (respuesta != 0);
+    }
+    static void showPatientMenu(){
+        int respuesta = 0;
+        do {
+            System.out.println("\n\n");
+            System.out.println(" Paciente");
+            System.out.println(" 1.- Libro de Citas");
+            System.out.println(" 2.- Mis Citas");
+            System.out.println(" 3.- Regresar");
+            Scanner sc = new Scanner(System.in);
+            respuesta = Integer.valueOf(sc.nextLine());
+            switch (respuesta) {
+                case 1:
+                    System.out.println("**libro de Citas ");
+                    break;
+                case 2:
+                    System.out.println("**Mi Cita Medica");
+                    break;
+                case 0:
+                    showMenu();
+                    break;
+                default:
+                    System.out.println("Por favor selecione una opcion corecta");
+            }
+
+
+        }while (respuesta != 0);
+
+    }
+}
