fcfe4664f561f776083bf7fde9acf07d6cb0c4e0
Jasson Caamal <jasson.adrian.caamal.medina@gmail.com>
2021-04-21 19:12:24 -0500

    Cambiando a estatico la variable id de Doctor para hacerlo autoincrementable

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,18 +1,23 @@
 public class Doctor {
 
     //atributos
-    int id;
+    static int id = 0;
     String name;
     String speciality;
 
     //comportamiento
     Doctor(){
         System.out.println("construyendo el metodo doctor");
+        id++;
     }
     Doctor(String name){
         System.out.println("el nombre del doctor es: " + name);
     }
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID= "+ id);
     }
 }
