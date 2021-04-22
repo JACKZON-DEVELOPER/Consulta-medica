898c97562715211f0f03821399f26b99ec921fc6
Jasson Caamal <jasson.adrian.caamal.medina@gmail.com>
2021-04-22 16:52:35 -0500

    atributos y constructor del metodo Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,17 @@
 public class Paciente {
+    //atributos
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+    //constructor
+    Paciente(String name, String email){
+        this.name=name;
+        this.email=email;
+    }
 }
